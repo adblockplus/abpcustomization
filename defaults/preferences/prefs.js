dd440496e443fd889c62ae9e0300ef25da17bc5e
Wladimir Palant <trev@adblockplus.org>
2012-01-23 16:35:09 +0100

    Moved Customizations extension to the new module framework

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,9 @@
+pref("extensions.abpcustomization.vertical-preferences-layout", false);
+pref("extensions.abpcustomization.preferences-one-line-subscriptions", false);
+pref("extensions.abpcustomization.preferences-remove-checkbox-label", false);
+pref("extensions.abpcustomization.preferences-remove-actions-button", false);
+pref("extensions.abpcustomization.preferences-no-italics", false);
+pref("extensions.abpcustomization.preferences-bold-titles", false);
+pref("extensions.abpcustomization.toolbar-icon-display", 0);
+pref("extensions.abpcustomization.green-icon", false);
+pref("extensions.abpcustomization.remove-menus", false);
