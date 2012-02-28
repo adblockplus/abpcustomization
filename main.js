54c9286821bedebeb41d3323272dfed31d35bb86
Wladimir Palant <trev@adblockplus.org>
2012-02-28 20:22:32 +0100

    Removed outdated parameter

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ var WindowFeature =
   init: function()
   {
     if (!this.observer)
-      this.observer = new WindowObserver(this, "ready", true);
+      this.observer = new WindowObserver(this, "ready");
   },
 
   shutdown: function()
