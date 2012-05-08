6888427ae637a136f06133bc1baf2c5115ab6f0e
Wladimir Palant <trev@adblockplus.org>
2012-05-08 22:56:07 +0200

    Backed out changeset 6add036e61f1 (Topic 9249 - Add option to disable italics in the Filter Preferences window), this is part of Adblock Plus now

--- main.js
+++ main.js
@@ -191,12 +191,6 @@ var RemoveActionsButton =
   stylesheet: "chrome://abpcustomization/content/noActionButton.css"
 };
 
-var NoItalics =
-{
-  __proto__: StylesheetFeature,
-  stylesheet: "chrome://abpcustomization/content/noItalics.css"
-};
-
 var ToolbarIconDisplay =
 {
   __proto__: StylesheetFeature,
@@ -245,7 +239,6 @@ let features =
   "preferences-one-line-subscriptions": OneLineSubscriptions,
   "preferences-remove-checkbox-label": RemoveCheckboxLabel,
   "preferences-remove-actions-button": RemoveActionsButton,
-  "preferences-no-italics": NoItalics,
   "toolbar-icon-display": ToolbarIconDisplay,
   "green-icon": GreenIcon,
   "remove-menus": RemoveMenus
