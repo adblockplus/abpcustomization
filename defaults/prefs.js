f364509cbc60294e4ed5cf8d0a9bdf58bb699853
Wladimir Palant <trev@adblockplus.org>
2015-05-18 21:07:34 +0200

    Issue 2545 - Update Customizations dependency on buildtools to revision 9069f8de837b

--- defaults/prefs.js
+++
@@ -1,8 +0,0 @@
-pref("extensions.abpcustomization.vertical-preferences-layout", false);
-pref("extensions.abpcustomization.preferences-one-line-subscriptions", false);
-pref("extensions.abpcustomization.preferences-remove-actions-button", false);
-pref("extensions.abpcustomization.preferences-no-italics", false);
-pref("extensions.abpcustomization.preferences-bold-titles", false);
-pref("extensions.abpcustomization.toolbar-icon-display", 0);
-pref("extensions.abpcustomization.green-icon", false);
-pref("extensions.abpcustomization.remove-menus", false);
